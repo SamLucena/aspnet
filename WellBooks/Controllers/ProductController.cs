80747c088ce2cca929ff4338de2ff04e07022704
Samuel Lucena de Abreu <samuellucenadeabreu@hotmail.com>
2022-06-03 16:21:44 -0300

    Product detail

--- WellBooks/Controllers/ProductController.cs
+++ WellBooks/Controllers/ProductController.cs
@@ -1,16 +1,34 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WellBooks.Data;
+using WellBooks.Models;
 
 namespace WellBooks.Controllers
 {
-    [Authorize(Policy = "EmployeeOnly")]
     public class ProductController : Controller
     {
-        public ActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
         {
             return View();
         }
 
+        [AllowAnonymous]
+        public IActionResult Details(int? id)
+        {
+            if (id == null) { return RedirectToAction("Index", "Home"); }
+            var product = _db.Products.Include(x => x.Category).FirstOrDefault(x => x.Id == id);
+            
+            return View(product);
+        }
+        //IFormCollection : Itens do formulário
     }
 }
